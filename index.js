d8f7e5307709c83c4c43f21c89005cfe1190b2b9
tonyonlian <540340102@qq.com>
2020-02-16 14:07:18 +0800

    增加一对一，一对多，自定义标量

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // 1. Require 'apollo-server'
 const {ApolloServer} = require('apollo-server');
+const {GraphQLScalarType} = require('graphql')
 const typeDefs = `
+        scalar DateTime
         type User {
             githubLogin: ID!
             name: String
             avatar: String
             postedPhotos: [Photo!]!
+            inPhotos: [Photo!]!
         }
         enum PhotoCategory {
             SELFIE
@@ -20,7 +23,9 @@ const typeDefs = `
             name: String!
             description: String
             category: PhotoCategory!
+            created: DateTime!
             postedBy: User!
+            taggedUsers: [User!]!
         }
         input PostPhotoInput {
             name: String!
@@ -49,21 +54,36 @@ let photos = [
         "name": "Dropping the Heart Chute",
         "description": "The heart chute is one of my favorite chutes",
         "category": "ACTION",
-        "githubUser":"gPlake"
+        "githubUser":"gPlake",
+        "created": "3-28-1977"
     },{
         "id": "2",
         "name": "Enjoying the sunshine",
         "category": "SELFIE",
-        "githubUser": "sSchmidt"
+        "githubUser": "sSchmidt",
+        "created": "1-2-1985"
     },
     {
         "id": "3",
         "name": "Gunbarrel 25",
         "description": "25 laps on gunbarrel today",
         "category": "LANDSCAPE",
-        "githubUser": "sSchmidt"
+        "githubUser": "sSchmidt",
+        "created": "2018-04-15T19:09:57.308Z"
     }
 ];
+
+let tags = [
+    {
+        "photoID": "1", "userID": "gPlake"
+    },{
+        "photoID": "2", "userID": "sSchmidt"
+    },{
+        "photoID": "2", "userID": "mHattrup"
+    },{
+        "photoID": "2", "userID": "gPlake"
+    }
+]
 const resolvers = {
     Query: {
         totalPhotos: () => photos.length,
@@ -74,7 +94,8 @@ const resolvers = {
             console.log(args);
             var newPhoto = {
                 id: _id++,
-                ... args.input
+                ... args.input,
+                created: new Date()
             }
             photos.push(newPhoto);
             return newPhoto;
@@ -84,13 +105,26 @@ const resolvers = {
         url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
         postedBy: parent => {
             return users.find(u => u.githubLogin === parent.githubUser )
-        }
+        },
+        taggedUsers: parent => tags.filter(tag => tag.photoID === parent.id)
+                                    .map(tag => tag.userID)
+                                    .map(userID => users.find(u => u.githubLogin === userID))
     },
     User: {
         postedPhotos: parent => {
             return photos.filter( p => p.githubUser === parent.githubLogin)
-        }
-    }
+        },
+        inPhotos: parent => tags.filter(tag => tag.userID === parent.id)
+                                .map(tag => tag.photoID)
+                                .map(photoID => photos.find(p => p.id === photoID))
+    },
+    DateTime: new GraphQLScalarType({
+        name: 'DateTime',
+        description: 'A valid date time value.',
+        parseValue: value => new Date(value),
+        serialize: value => new Date(value).toISOString(),
+        parseLiteral: ast => ast.value
+    })
 
 };
 
