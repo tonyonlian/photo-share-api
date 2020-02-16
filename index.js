96da5db11105c0e222354997213a37fa7d735a03
tonyonlian <540340102@qq.com>
2020-02-16 15:47:17 +0800

    重构project to express

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // 1. Require 'apollo-server'
-const {ApolloServer} = require('apollo-server');
-const {GraphQLScalarType} = require('graphql')
+const {ApolloServer} = require('apollo-server-express');
+const express = require('express');
+const {GraphQLScalarType} = require('graphql');
+const expressPlayground = require('graphql-playground-middleware-express').default
+
+var app = express();
+
+
 const typeDefs = `
         scalar DateTime
         type User {
@@ -128,16 +134,17 @@ const resolvers = {
 
 };
 
-
-//2.Create a new instance of the server.
-//3.Send it an object with typeDefs (the schema) and resolvers
-
 const server = new ApolloServer({
     typeDefs,
     resolvers
-})
-//4. Call listen on the server to launch the web server
-server.listen()
-    .then(
-        ({url}) => console.log(`GraphQL service running on ${url}`)
-    );
+});
+
+server.applyMiddleware({app});
+app.get("/",(req,res) => res.send('Welcome to  the PhotoShare API'));
+app.get("/playground",expressPlayground({endpoint: '/graphql'}));
+
+app.listen({port: 4000},() =>{
+    console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
+});
+
+//mongodb+srv://tunyl:<password>@cluster0-4n2ii.mongodb.net/test?retryWrites=true&w=majority
